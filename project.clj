4318310c8c6c69eabe1ece2d8efe82b47f66b544
Zach Tellman <ztellman@gmail.com>
2014-08-01 17:20:49 -0700

    mark 0.1.0

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject clj-radix "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-radix "0.1.0"
+  :description "A radix tree"
   :url "https://github.com/ztellman/clj-radix"
   :license {:name "MIT License"
             :url "http://dd.mit-license.org/"}
-  :dependencies [[primitive-math "0.1.4-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0-beta1"]
+  :dependencies [[primitive-math "0.1.4"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [criterium "0.4.3"]
-                                  [collection-check "0.1.2"]
-                                  [org.clojure/math.combinatorics "0.0.7"]
+                                  [collection-check "0.1.3"]
+                                  [org.clojure/math.combinatorics "0.0.8"]
                                   #_[codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
   :plugins [[codox "0.8.0"]]
   :codox {;:writer codox-md.writer/write-docs
